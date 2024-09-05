b58e5fe403d05740b0f2c8f6e2928954bcea508b
ChanUi <hcu55@naver.com>
2024-09-05 17:23:24 +0900

    :tada: First init

---
+++ src/main/java/woori/petmily_card/PetmilyCardApplication.java
@@ -0,0 +1,13 @@
+package woori.petmily_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmilyCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetmilyCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woori/petmily_card/PetmilyCardApplicationTests.java
@@ -0,0 +1,13 @@
+package woori.petmily_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmilyCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
