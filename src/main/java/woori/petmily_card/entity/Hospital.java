14ea7a5733144d6674880e3f8b7f20a1d8cb07b6
qbobl5 <ddddd991125@gmail.com>
2024-09-06 10:13:31 +0900

    feat: 필요한 entity 추가

---
+++ src/main/java/woori/petmily_card/entity/Hospital.java
@@ -0,0 +1,28 @@
+package woori.petmily_card.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Hospital {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int hospitalNo;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int sale;
+
+    @Column(nullable = false, columnDefinition = "true")
+    private boolean status;
+}
