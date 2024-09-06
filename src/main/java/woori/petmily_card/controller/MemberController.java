d8c5ef9f533b0d7568b2bff2f2d1e04497aebd6b
ChanUi <hcu55@naver.com>
2024-09-06 12:20:28 +0900

    feat: 회원가입, 로그인 controller 추가

---
+++ src/main/java/woori/petmily_card/controller/MemberController.java
@@ -0,0 +1,60 @@
+package woori.petmily_card.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import woori.petmily_card.dto.MemberDto;
+import woori.petmily_card.entity.Member;
+import woori.petmily_card.service.MemberService;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원가입 페이지
+    @GetMapping("/register")
+    public ModelAndView showRegisterPage() {
+        return new ModelAndView("register");
+    }
+
+    // 회원가입 처리
+    @PostMapping("/register")
+    public ModelAndView register(MemberDto memberDto) {
+        memberService.register(memberDto);
+        return new ModelAndView("redirect:/login");
+    }
+
+    // 로그인 페이지
+    @GetMapping("/login")
+    public ModelAndView showLoginPage() {
+        return new ModelAndView("login");
+    }
+
+    // 로그인 처리
+    @PostMapping("/login")
+    public ModelAndView login(MemberDto memberDto, HttpSession session, Model model) {
+        Member member = memberService.login(memberDto);
+        if (member != null) {
+            session.setAttribute("member", member);
+            return new ModelAndView("redirect:/transaction");
+        } else {
+            ModelAndView mav = new ModelAndView("login");
+            model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
+            return mav;
+        }
+    }
+
+    // 로그아웃 처리
+    @GetMapping("/logout")
+    public ModelAndView logout(HttpSession session) {
+        session.invalidate();
+        return new ModelAndView("redirect:/login");
+    }
+}
