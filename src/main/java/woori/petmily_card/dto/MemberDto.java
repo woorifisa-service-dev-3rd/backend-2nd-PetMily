6cecdaeb44ec6685d96797cad26357f0e78bc67a
ChanUi <hcu55@naver.com>
2024-09-06 16:30:07 +0900

    fix: MemberDto 생성자 주입 수정

--- src/main/java/woori/petmily_card/dto/MemberDto.java
+++ src/main/java/woori/petmily_card/dto/MemberDto.java
@@ -1,13 +1,22 @@
 package woori.petmily_card.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class MemberDto {
     private String name;
     private String id;
     private String password;
     private String phoneNumber;
+
+    private MemberDto(String name, String id, String password, String phoneNumber) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static MemberDto of(String name, String id, String password, String phoneNumber) {
+        return new MemberDto(name, id, password, phoneNumber);
+    }
 }
