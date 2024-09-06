942c166e12a4285b263c7c59b2a413426b385df3
qbobl5 <ddddd991125@gmail.com>
2024-09-06 14:39:23 +0900

    feat: 거래내역 조회를 위한 controller 메소드 추가

--- src/main/java/woori/petmily_card/controller/TransactionController.java
+++ src/main/java/woori/petmily_card/controller/TransactionController.java
@@ -2,7 +2,9 @@ package woori.petmily_card.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import woori.petmily_card.service.TransactionService;
@@ -24,9 +26,10 @@ public class TransactionController {
         return "tmp";
     }
 
-    @GetMapping("/show")
-    public String showTransactions(int cardNo, int page) {
-        transactionService.show(cardNo, page);
-        return "";
+    @GetMapping("/show/{cardNo}/{page}")
+    public String showTransactions(@PathVariable(value="cardNo") int cardNo,
+                                   @PathVariable(value="page") int page, Model model) {
+        model.addAttribute("transactions", transactionService.show(cardNo, page));
+        return "showTransaction";
     }
 }
