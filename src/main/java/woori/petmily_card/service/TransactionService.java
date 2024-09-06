afa7b52372945605725b6b05c85e4423a30827be
qbobl5 <ddddd991125@gmail.com>
2024-09-06 12:41:37 +0900

    feat: 거래내역 조회를 위한 service 메소드 추가

--- src/main/java/woori/petmily_card/service/TransactionService.java
+++ src/main/java/woori/petmily_card/service/TransactionService.java
@@ -1,5 +1,11 @@
 package woori.petmily_card.service;
 
+import woori.petmily_card.dto.TransactionResponse;
+
+import java.util.List;
+
 public interface TransactionService {
     void save(int cardNo, int hospitalNo, int amount);
+
+    List<TransactionResponse> show(int cardNo, int page);
 }
