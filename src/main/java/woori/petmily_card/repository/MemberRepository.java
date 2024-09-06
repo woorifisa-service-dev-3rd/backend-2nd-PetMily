83847c6f6035cc751d48c4b68be85985ce704c63
ChanUi <hcu55@naver.com>
2024-09-06 12:18:51 +0900

    feat: 회원가입, 로그인에 필요한 repository 추가

---
+++ src/main/java/woori/petmily_card/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package woori.petmily_card.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import woori.petmily_card.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Member findByIdAndPassword(String id, String password);
+}
