044c1427865f61aa99891138c4edad30b5ce3681
ChanUi <hcu55@naver.com>
2024-09-06 19:13:50 +0900

    fix: MemberRepository의 findByIdAndPassword 인터페이스 메서드 삭제

--- src/main/java/woori/petmily_card/repository/MemberRepository.java
+++ src/main/java/woori/petmily_card/repository/MemberRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import woori.petmily_card.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Member findByIdAndPassword(String id, String password);
     Member findById(String id);
 }
