513e3a0f3add08abd5fbe41c0395587612ddbff3
ChanUi <hcu55@naver.com>
2024-09-07 17:40:36 +0900

    fix: MemberController 로그아웃 PostMapping 변경

--- src/main/java/woori/petmily_card/controller/MemberController.java
+++ src/main/java/woori/petmily_card/controller/MemberController.java
@@ -19,26 +19,22 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // 회원가입 페이지
     @GetMapping("/register")
     public ModelAndView showRegisterPage() {
         return new ModelAndView("member/register");
     }
 
-    // 회원가입 처리
     @PostMapping("/register")
     public ModelAndView register(MemberDto memberDto) {
         memberService.register(memberDto);
         return new ModelAndView("redirect:/petmily/members/login");
     }
 
-    // 로그인 페이지
     @GetMapping("/login")
     public ModelAndView showLoginPage() {
         return new ModelAndView("member/login");
     }
 
-    // 로그인 처리
     @PostMapping("/login")
     public ModelAndView login(MemberDto memberDto, HttpSession session) {
         Member member = memberService.login(memberDto);
@@ -46,8 +42,7 @@ public class MemberController {
         return new ModelAndView("redirect:/petmily/main");
     }
 
-    // 로그아웃 처리
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public ModelAndView logout(HttpSession session) {
         session.invalidate();
         return new ModelAndView("redirect:/petmily/members/login");
