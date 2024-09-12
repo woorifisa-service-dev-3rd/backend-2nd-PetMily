b779155280c3cc0e1a94ff01c09c9e4de1672978
HoChoRoo <rttkhc0304@gmail.com>
2024-09-12 10:50:01 +0900

    feat: Board DTO 생성 - 보드 목록에 들어갈 보드 정보 response - 보드 페이징 처리하여 보여줄 response

---
+++ src/main/java/woori/petmily_card/dto/BoardPageResponse.java
@@ -0,0 +1,25 @@
+package woori.petmily_card.dto;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+import woori.petmily_card.entity.Board;
+
+import java.util.List;
+
+@Getter
+public class BoardPageResponse {
+    private List<BoardResponse> boards;
+    private int currentPage;
+    private int totalPage;
+
+    private BoardPageResponse(List<BoardResponse> boards, int currentPage, int totalPage) {
+        this.boards = boards;
+        this.currentPage = currentPage;
+        this.totalPage = totalPage;
+    }
+
+    public static BoardPageResponse from(Page<Board> boards) {
+        return new BoardPageResponse(boards.map(BoardResponse::from).getContent(),
+                boards.getNumber() + 1, boards.getTotalPages());
+    }
+}
--- src/main/java/woori/petmily_card/dto/BoardResponse.java
+++ src/main/java/woori/petmily_card/dto/BoardResponse.java
@@ -2,38 +2,26 @@ package woori.petmily_card.dto;
 
 import lombok.Getter;
 import woori.petmily_card.entity.Board;
-import woori.petmily_card.entity.Comment;
-import woori.petmily_card.entity.Member;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 public class BoardResponse {
 
     private int boardNo;
-    private Member member;
+    private String member;
     private String title;
-    private String content;
-    private List<Comment> comments;
     private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
-    public BoardResponse(int boardNo, Member member, String title,
-                         String content,List<Comment> comments,
-                         LocalDateTime createdAt, LocalDateTime updatedAt) {
+    private BoardResponse(int boardNo, String member, String title, LocalDateTime createdAt) {
         this.boardNo = boardNo;
         this.member = member;
         this.title = title;
-        this.content = content;
-        this.comments = comments;
         this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 
     public static BoardResponse from(Board board){
-        return new BoardResponse(board.getBoardNo(),board.getMember(),
-                board.getTitle(), board.getContent(),board.getComments(),
-                board.getCreatedAt(),board.getUpdatedAt());
+        return new BoardResponse(board.getBoardNo(), board.getMember().getName(),
+                board.getTitle(), board.getCreatedAt());
     }
 }
