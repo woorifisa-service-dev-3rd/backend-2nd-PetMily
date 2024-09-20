6b8cdacfd779c362a3b1fd826ecefeeb95b22c0a
안찬웅 <1stevering@naver.com>
2024-09-20 15:53:47 +0900

    fix: MemberRepository merge conflit 오류 해결

--- src/main/java/woori/petmily_card/repository/MemberRepository.java
+++ src/main/java/woori/petmily_card/repository/MemberRepository.java
@@ -3,8 +3,6 @@ package woori.petmily_card.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import woori.petmily_card.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member,Integer> {
-
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
